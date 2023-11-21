d1c7f265ea9a7c0aab547fc5a18bd8575ffd622d
Anthony Miller <ant.miller@rocketmail.com>
2023-11-20 22:01:18 -0800

    Delete js directory

--- js/script.js
+++
@@ -1,9 +0,0 @@
-
-const closeAccountButton = document.getElementById("closeAccountBtn");
-
-closeAccountButton.addEventListener("click", () => {
-  const confirmation = confirm("Are you sure you want to close this account?");
-
-  if (confirmation) {
-  }
-});
